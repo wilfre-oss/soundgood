9bc5bbe5e7fb4e3b558bc5e3b70e2ef4a7352702
wilfre-oss <63850794+wilfre-oss@users.noreply.github.com>
2022-12-15 19:12:58 +0100

    rented

--- src/main/java/se/kth/iv1351/soundgoodjdbc/controller/Controller.java
+++ src/main/java/se/kth/iv1351/soundgoodjdbc/controller/Controller.java
@@ -36,17 +36,18 @@ public class Controller {
         }
 
         List<Instrument> instruments = soundgoodDAO.findAllInstruments();
-        for(Instrument i : instruments){
-            if(i.getId() == Integer.parseInt(instrumentId))
-                return "Instrument already rented";
-        }
 
         try{
-            soundgoodDAO.createRental(Integer.parseInt(studentId), Integer.parseInt(instrumentId));
-            return "Rental successfully added";
+            for(Instrument i : instruments){
+                if(i.getId() == Integer.parseInt(instrumentId)){
+                    soundgoodDAO.createRental(Integer.parseInt(studentId), Integer.parseInt(instrumentId));
+                    return "Rental successfully added";
+                }
+            }
         } catch (SgException e) {
             throw new SgException(e.getMessage(),e);
         }
+        return "Instrument already rented";
     }
 
     public void terminateRental(String rental){
